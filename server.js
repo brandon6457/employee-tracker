6d47b181a88c764ed9a9e5eaf705dab2737ded40
brandon6457 <brandon6457@gmail.com>
2023-06-04 09:52:46 -0400

    finished server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const db = mysql.createConnection(
    {
       host: 'localhost',
       user: 'root',
-      password: 'rootroot'
+      password: 'rootroot',
       database: 'employee_db'
    },
    console.log(`Connected to the employee_db database.`)
@@ -121,4 +121,105 @@ const displayTables = (questions) => {
          });
       });
    }
-   else if 
+   else if (questions === "Add an employee") {
+      Promise.all([
+         db.promise().query(`SELECT * FROM role`),
+         db.promise().query(`SELECT* FROM employee WHERE manager_id IS NULL`)
+      ])
+      .then(([roleRows, employeeRows]) => {
+         const roles = roleRows[0];
+         const roleChoices = roles.map(({ id, title }) => ({
+            name: title,
+            value: id
+         }));
+         managerChoices.push({ name: 'None', value: null });
+         
+         return inquirer.prompt([
+            {
+               type: 'input',
+               message: "What is the new employee's first name?",
+               name: 'firstName',
+            },
+            {
+               type: 'input',
+               message: "What is the new employee's last name?",
+               name: 'lastName',
+            },
+            {
+               type: 'list',
+               message: "What is the new employee's role?",
+               choices: roleChoices,
+               name: 'roleId',
+            },
+            {
+               type: 'list',
+               message: "Who is the new employee's manager?",
+               choices: managerChoices,
+               name: 'managerId',
+            },
+         ]);
+      })
+      .then ((answers) => {
+         const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+         const params = [answers.firstName, answers.lastName, answers.roleId, answers.managerId];
+
+         db.query(sql, params, function (err, results) {
+            if (err) {
+               console.error(err);
+            } else {
+               console.log(`Added ${answers.firstName} ${answers.lastName} to employees.`);
+            }
+         });
+      });
+   }
+   else if (questions === "Update an employee role") {
+      Promise.all([
+         db.promise().query(`SELECT * FROM employee`),
+         db.promise().query(`SELECT *FROM role`)
+      ])
+      .then(([employeeRows, rolesRows]) => {
+         const employees = employeeRows[0];
+         const employeeChoices = employees.map(({ id, first_name, last_name }) => ({
+            name: `${first_name} ${last_name}`,
+            value: id
+         }));
+
+         const roles = rolesRows[0];
+         const roleChoices = roles.map(({ id, title }) => ({
+            name: title,
+            value: id
+         }));
+
+         return inquirer.prompt([
+            {
+               type: 'list',
+               message: "Which employee's role do you want to update?",
+               choices: employeeChoices,
+               name: 'employeeId',
+            },
+            {
+               type: 'list',
+               message: "What is the employee's new role?",
+               choices: roleChoices,
+               name: 'roleId',
+            },
+         ]);
+      })
+      .then((answers) => {
+         const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+         const params = [answers.roleId, answers.employeeId];
+
+         db.query(sql, params, function (err, results) {
+            if (err) {
+               console.error(err);
+            } else {
+               console.log(`Updated employee ${answers.employeeId} with new role ${answers.roleId}.`);
+            }
+         });
+      });
+   }
+}
+
+app.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`);
+});
